3d9066fdf3b3e5504b496f7473311b25b8d9cac0
Fernando Palma <ferpalma21@gmail.com>
2017-06-04 17:47:31 +0200

    almost there

--- rover.js
+++ rover.js
@@ -1,4 +1,3 @@
-
 //Lets define our planet first :) MARS!!!
 //Grid 10*10 2 arrays made with 2 "for loops"
 var grid=[];
@@ -10,68 +9,50 @@ for (var y = 0; y < 10; y++) {
 
 console.log(grid);
 
-//Mars is a planet (sphere) so after 10 goes back to 0
-//Declare variables to do roundTrip in X and Y (2 variables)
-/*
-var roundTripX= rover.position[0];
-var roundTripY= rover.position[1];
-
-//Lets start with X axle
-if (roundTripX > 10){
-  rover.position[0]=0;
-}else if (roundTripX < 0) {
-  rover.position[0]=10;
-}
-
-//Now Y axle
-if (roundTripY > 10) {
-  rover.position[1]=0;
-}else if (roundTripY < 0) {
-  rover.position[1]=10;
-}
-*/
 //Create our rover
-
 var myRover = {
-  position: [0,0],
+  position: [0, 0],
   direction: 'N'
 };
+console.log(myRover);
+
 
 
 //Default code
 function goForward(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]++;
-      break;
-    case 'E':
       rover.position[1]++;
       break;
+    case 'E':
+      rover.position[0]++;
+      break;
     case 'S':
-      rover.position[0]--;
+      rover.position[1]--;
       break;
     case 'W':
-      rover.position[1]--;
+      rover.position[0]--;
       break;
   }
 
   console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
 }
 goForward(myRover);
+
 //Same code for forward but inverted
 function goBack(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]--;
-      break;
-    case 'E':
       rover.position[1]--;
       break;
+    case 'E':
+      rover.position[0]--;
+      break;
     case 'S':
-      rover.position[0]++;
+      rover.position[1]++;
       break;
     case 'W':
-      rover.position[1]++;
+      rover.position[0]++;
       break;
   }
   console.log("New Rover Position: [" + rover.position[0] +", "+ rover.position[1] + "]");
@@ -123,15 +104,45 @@ console.log("New Rover Direction: [" + rover.direction + "]");
 turnLeft(myRover);
 
 
-while (true) {
-  var controller= prompt("Forward/Backward/Right/Left?");
+//Mars is a planet (sphere) so after 10 goes back to 0
+//Declare variables to do roundTrip in X and Y (2 variables)
+
+function roundTrip(rover) {
+
+  //Lets start with X axle
+var roundTripX = rover.position[0];
+  if (roundTripX > 10){
+    roundTripX = 0;
+  } else if (roundTripX < 0) {
+    roundTripX = 10;
+  }
+//Now Y axle
+var roundTripY = rover.position[1];
+  if (roundTripY > 10) {
+    roundTripY = 0;
+  }else if (roundTripY < 0) {
+    roundTripY = 10;
+  }
+  //print out the position after the overlap setting
+      console.log("New Rover Position: [" + roundTripX + ", " + roundTripY + "]");
+}
+roundTrip(myRover);
+
+//while
+
+
+  var controller = prompt("Forward/Backward/Right/Left?");
+    controller = controller.toUpperCase();
     if (controller === 'F'){
       goForward(myRover);
-    }else if (controller ==='B') {
+      roundTrip(myRover);
+    } else if (controller ==='B') {
       goBack(myRover);
-    }else if (controller === 'R') {
+      roundTrip(myRover);
+    } else if (controller === 'R') {
       turnRight(myRover);
-    }else if (Controller === 'L'){
+      roundTrip(myRover);
+    } else if (controller === 'L'){
       turnLeft(myRover);
+      roundTrip(myRover);
     }
-}
