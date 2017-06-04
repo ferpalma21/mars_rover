7827017601fd1c9b8590ed165166fecdc3b25ec6
Fernando Palma <ferpalma21@gmail.com>
2017-06-04 22:49:35 +0200

    Starting with Bonus

--- rover.js
+++ rover.js
@@ -1,13 +1,5 @@
-//Lets define our planet first :) MARS!!!
-//Grid 10*10 2 arrays made with 2 "for loops"
-var grid=[];
-for (var x = 0; x < 10; x++) {
-  grid[x]=[];
-for (var y = 0; y < 10; y++) {
-  grid[x][y]=[];
-}}
 
-console.log(grid);
+
 
 //Create our rover
 var myRover = {
@@ -15,7 +7,17 @@ var myRover = {
   direction: 'N'
 };
 console.log(myRover);
+//Lets define our planet first :) MARS!!!
+//Grid 10*10 2 arrays made with 2 "for loops"
+var grid=[];
 
+for (var x = 0; x <= 10; x++) {
+  (myRover.position[0])=[x];
+for (var y = 0; y <= 10; y++) {
+  (myRover.position[1])=[y];
+}}
+
+console.log(grid);
 
 
 //Default code
@@ -107,42 +109,50 @@ turnLeft(myRover);
 //Mars is a planet (sphere) so after 10 goes back to 0
 //Declare variables to do roundTrip in X and Y (2 variables)
 
-function roundTrip(rover) {
+function roundTrip(rover) {  //Lets start with X axle
 
-  //Lets start with X axle
 var roundTripX = rover.position[0];
-  if (roundTripX > 10){
-    roundTripX = 0;
+if (roundTripX > 10){
+    roundTripX = rover.position[0]=0;
   } else if (roundTripX < 0) {
-    roundTripX = 10;
+    roundTripX = rover.position[0]=10;
   }
+
 //Now Y axle
 var roundTripY = rover.position[1];
   if (roundTripY > 10) {
-    roundTripY = 0;
+    roundTripY = rover.position[1]=0;
   }else if (roundTripY < 0) {
-    roundTripY = 10;
+    roundTripY = rover.position[1]=10;
   }
   //print out the position after the overlap setting
       console.log("New Rover Position: [" + roundTripX + ", " + roundTripY + "]");
 }
 roundTrip(myRover);
 
-//while
 
 
-  var controller = prompt("Forward/Backward/Right/Left?");
-    controller = controller.toUpperCase();
-    if (controller === 'F'){
-      goForward(myRover);
-      roundTrip(myRover);
-    } else if (controller ==='B') {
-      goBack(myRover);
-      roundTrip(myRover);
-    } else if (controller === 'R') {
-      turnRight(myRover);
-      roundTrip(myRover);
-    } else if (controller === 'L'){
-      turnLeft(myRover);
-      roundTrip(myRover);
-    }
+//Users Input
+var usersInput = prompt("Forward/Backward/Right/Left?");
+      var usersInputToUpperCase = usersInput.toUpperCase();
+console.log(usersInputToUpperCase);
+var text =[];
+
+  for (i = 0 ; i  < usersInputToUpperCase.length ; i++) {
+      text = usersInputToUpperCase[i];
+      //translate user's input into array
+
+                if (text === 'F'){
+                  goForward(myRover);
+                  roundTrip(myRover);
+                } else if (text ==='B') {
+                  goBack(myRover);
+                  roundTrip(myRover);
+                } else if (text === 'R') {
+                  turnRight(myRover);
+                  roundTrip(myRover);
+                } else if (text === 'L'){
+                  turnLeft(myRover);
+                  roundTrip(myRover);
+                }
+}
